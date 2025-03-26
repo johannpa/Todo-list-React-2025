fad68c711c5830544a381c497ae446b5d582d363
johannpa <johann_paque@hotmail.fr>
2025-03-26 21:08:05 +0100

    Create and add the tailwind.config.js file did work and we can't use tailwind/form plugin for the moment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import forms from '@tailwindcss/forms';
+
 
 export default {
     // tailwind.config.js
@@ -10,7 +10,7 @@ export default {
       // ...
     },
     plugins: [
-      forms,
+      require('@tailwindcss/forms'),
       // ...
     ],
 }
