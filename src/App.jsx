48d7e175a76a59033c265c7ef4c38f6e58adc6fd
johannpa <johann_paque@hotmail.fr>
2025-03-26 22:15:49 +0100

    Create a new folder for the components, a new component, import nanoid for the id of the items and a function to delete items

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import ListItem from './components/ListItem';
 
 
 function App() {
-  
+  const [todoList, setTodoList] = useState([
+    { id: nanoid(8), content: "item 1" },
+    { id: nanoid(8), content: "item 2" },
+    { id: nanoid(8), content: "item 3" },
+  ]);
+
+  function deleteTodo(id) {
+    setTodoList(todoList.filter((item) => item.id !== id));
+  }
 
   return (
     <div className="h-screen bg-slate-900">
@@ -10,12 +20,20 @@ function App() {
         <h1 className="text-3xl txt-slate-100 mb-4">La To-do liste</h1>
         <form action="" className="mb-10">
           <label htmlFor="todo-item" className="text-slate-50">Ajouter une chose à faire</label>
-          <input type="text" className="mt-1 block w-full rounded" name="" id="" />
-
+          <input 
+            type="text" 
+            className="mt-1 block w-full rounded" 
+            name="" 
+            id="" 
+            />
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
         <ul>
           
+          {todoList.map((item) => (
+            <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo}/>
+          ))}
+
         </ul>
       </div>
     </div>
