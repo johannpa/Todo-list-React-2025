a499da7c1f302c0572c35c8274ba6f56fd9f487e
johannpa <johann_paque@hotmail.fr>
2025-03-28 21:58:06 +0100

    Create a new state and a function for the validation of the form

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,32 @@ function App() {
   ]);
 
   const [todo, setTodo] = useState('');
+  const [showValidation, setShowValidation] = useState(false);
 
   function deleteTodo(id) {
     setTodoList(todoList.filter((item) => item.id !== id));
+  }
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    if(todo === '') {
+      setShowValidation(true);
+      return;
+    }
+
+    setTodoList([...todoList, { id: nanoid(8), content: todo }]);
+    setTodo('');
+    setShowValidation(false);
   }
 
   return (
     <div className="h-screen bg-slate-900">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl txt-slate-100 mb-4">La To-do liste</h1>
-        <form action="" className="mb-10">
+        <form action="" onSubmit={handleSubmit} className="mb-10">
           <label htmlFor="todo-item" className="text-slate-50">Ajouter une chose à faire</label>
           <input
             value={todo}
@@ -30,6 +46,9 @@ function App() {
             name="" 
             id="" 
             />
+            {showValidation && (
+              <p className="text-red-500">Ajoutez d'abord du contenu à votre tâche</p>
+            )}
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
         <ul>
